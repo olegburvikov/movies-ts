6c1eb83d01aed92da382d37ad3c970af5d49a303
olegburvikov <olegburvikov@gmail,com>
2021-01-16 17:52:35 +0200

    update Logo url

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import logoPng from '../../assets/img/logo.svg';
+import { Link } from 'react-router-dom'
+import logoPng from '../../assets/img/logo.svg'
 export default function Logo() {
   return (
     <div className="logo">
-      <Link to="/movies"><img src={logoPng} alt="company logo"/></Link>
+      <Link to="/">
+        <img src={logoPng} alt="company logo" />
+      </Link>
     </div>
   )
 }
