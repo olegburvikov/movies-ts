7de15704e7d64a9d1e3e83270630292114e2a6fa
olegburvikov <oleg.burvikov@alphanavtech.com>
2021-04-25 23:06:22 +0300

    update main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,13 +3,11 @@ import Main from '../components/Main/Main'
 
 const MainPage: React.FC = () => {
   return (
-    <>
-      <div className="full-page-centered">
-        <div className="container">
-          <Main />
-        </div>
+    <div className="full-page-centered">
+      <div className="container">
+        <Main />
       </div>
-    </>
+    </div>
   )
 }
 
