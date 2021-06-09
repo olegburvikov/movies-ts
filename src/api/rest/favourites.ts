57f12de6ed63d640d16196a3fc0ed4212faaf1f9
olegburvikov <olegburvikov@gmail,com>
2021-06-09 19:24:23 +0300

    update favourites.ts

--- src/api/rest/favourites.ts
+++ src/api/rest/favourites.ts
@@ -2,16 +2,16 @@ import { api } from '../index'
 import { IMovie } from '../../types/movie'
 import { formatISO } from '../../helpers/time'
 
-export const deleteFavourite = async (imdbID: string) => {
-  return await api.delete(`favourites/?imdbID=${imdbID}/`)
-}
-
 export const checkIsFavourite = async (imdbId: string) => {
-  return await api.get(`favourites/is-movie-favourite/?imdbID=${imdbId}/`)
+  return await api.get(`favourites/is-movie-favourite/?imdbID=${imdbId}`)
 }
 
 export const getFavourites = async () => {
   return await api.get(`favourites/`)
+}
+
+export const deleteFavourite = async (imdbID: string) => {
+  return await api.delete(`favourites/?imdbID=${imdbID}`)
 }
 
 export const postFavoriteMovie = async (data: IMovie) => {
